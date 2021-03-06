b03a904c11eb796b09b873d699306e663e607ae6
Chris Sev <chris@better.dev>
2021-03-06 12:42:50 -0800

    taking a break

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,26 @@ import CountrySearch from "../components/CountrySearch";
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Find Parity</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-600 to-purple-900">
+      <div className="px-8 lg:max-w-7xl mx-auto">
+        <Head>
+          <title>Find Parity</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className="grid grid-cols-2">
-        <div>
-          <h2>Find Parity</h2>
-          <p>
-            A simple tool to calculate the discount your country should get on
-            products that offer <a href="#">purchasing power parity</a>.
-          </p>
-        </div>
-        <div>
-          <CountrySearch />
-        </div>
-      </main>
+        <main className="grid lg:grid-cols-2 gap-20">
+          <div>
+            <h2>Find Parity</h2>
+            <p>
+              A simple tool to calculate the discount your country should get on
+              products that offer <a href="#">purchasing power parity</a>.
+            </p>
+          </div>
+          <div>
+            <CountrySearch />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
